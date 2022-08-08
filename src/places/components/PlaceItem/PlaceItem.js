77ca0b3d2da1b1bd1ffeec72e061c9c06ce648d9
veranika-karpava <veranika.karpava@gmail.com>
2022-08-08 15:40:39 -0700

    Build modal for view map

--- src/places/components/PlaceItem/PlaceItem.js
+++ src/places/components/PlaceItem/PlaceItem.js
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 import './PlaceItem.scss';
 import Card from '../../../shared/components/Card/Card';
 import Button from '../../../shared/components/Button/Button';
+import Modal from '../../../shared/components/Modal/Modal';
 
 const PlaceItem = ({ id, image, title, description, address, creatorId, coordinates }) => {
+    const [showMap, setShowMap] = useState(false);
+
+    // handler for modal with map
+    const openMapHandler = () => setShowMap(true);
+    const closeMapHandler = () => setShowMap(false);
+
     return (
-        <li className='user-places__item'>
-            <Card className='user-places__card'>
-                <div className='user-places__view'>
-                    <img src={image} alt={title} className='user-places__image' />
+        <>
+            <Modal show={showMap}
+                onCancel={closeMapHandler}
+                header={address}
+                contentClass='place-item-contents'
+                footerClass='place-item-actions'
+                footer={<Button onClick={closeMapHandler}>CLOSE</Button>}>
+                <div className='modal__map-container'>
+                    <h2>The MAP!</h2>
                 </div>
-                <div className='user-places__info'>
-                    <h2 className='user-places__name'>{title}</h2>
-                    <h3 className='user-places__address'>{address}</h3>
-                    <p className='user-places__description'>{description}</p>
-                </div>
-                <div className='user-places__actions'>
-                    <Button inverse > VIEW ON MAP </Button>
-                    <Button to={`places/${id}`} > EDIT </Button>
-                    <Button danger>DELETE</Button>
-                </div>
-            </Card>
-        </li>
+            </Modal>
+            <li className='user-places__item'>
+                <Card className='user-places__card'>
+                    <div className='user-places__view'>
+                        <img src={image} alt={title} className='user-places__image' />
+                    </div>
+                    <div className='user-places__info'>
+                        <h2 className='user-places__name'>{title}</h2>
+                        <h3 className='user-places__address'>{address}</h3>
+                        <p className='user-places__description'>{description}</p>
+                    </div>
+                    <div className='user-places__actions'>
+                        <Button inverse onClick={openMapHandler} > VIEW ON MAP </Button>
+                        <Button to={`places/${id}`} > EDIT </Button>
+                        <Button danger>DELETE</Button>
+                    </div>
+                </Card>
+            </li>
+        </>
     );
 };
 
