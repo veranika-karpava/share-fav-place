217048236f35e751f87696a31f275c8e379689f9
veranika-karpava <veranika.karpava@gmail.com>
2023-01-15 02:04:13 -0800

    Create data for dynamic selection icon from reac-icon library

---
+++ client/src/data/icons.data.js
@@ -0,0 +1,31 @@
+import {
+    FaHtml5,
+    FaCss3Alt,
+    FaSass,
+    FaReact,
+    FaNodeJs,
+    FaHeart,
+    FaDocker,
+} from 'react-icons/fa';
+import { CgMenu } from "react-icons/cg";
+import { FiHeart } from 'react-icons/fi';
+import { AiOutlineHome, AiOutlineUser, AiOutlineFileAdd, AiOutlineGroup } from 'react-icons/ai';
+import { RiLogoutBoxRLine } from 'react-icons/ri';
+
+
+export const icons = {
+    FaHtml5: FaHtml5,
+    FaCss3Alt: FaCss3Alt,
+    FaSass: FaSass,
+    FaReact: FaReact,
+    FaNodeJs: FaNodeJs,
+    FaHeart: FaHeart,
+    FaDocker: FaDocker,
+    CgMenu: CgMenu,
+    FiHeart: FiHeart,
+    AiOutlineHome: AiOutlineHome,
+    AiOutlineUser: AiOutlineUser,
+    AiOutlineGroup: AiOutlineGroup,
+    AiOutlineFileAdd: AiOutlineFileAdd,
+    RiLogoutBoxRLine: RiLogoutBoxRLine,
+};
